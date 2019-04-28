d08502e4f3b4d9091dc9ed69a5554dcaf2b0633b
hiuron <hiuron@sina.com>
2019-04-28 15:05:17 +0800

    ISynchronizeManager

--- src/UserManage.Core/SynchronizeCore/DomainService/ISynchronizeManager.cs
+++ src/UserManage.Core/SynchronizeCore/DomainService/ISynchronizeManager.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using UserManage.AbpExternalCore;
 
 namespace UserManage.SynchronizeCore.DomainService
 {
@@ -10,6 +12,11 @@ namespace UserManage.SynchronizeCore.DomainService
     /// </summary>
     public interface ISynchronizeManager : IDomainService
     {
-
+        /// <summary>
+        /// 同步单个组织
+        /// </summary>
+        /// <param name="wx_dept"></param>
+        /// <returns>更新的本地Id</returns>
+        Task<long?> MatchSingleDepartment(AbpWeChatDepartment wx_dept);
     }
 }
