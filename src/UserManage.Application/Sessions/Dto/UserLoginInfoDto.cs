05a7885cca9843bda9a2001a1e39bda912b903b2
hiuron <hiuron@sina.com>
2019-05-30 16:08:28 +0800

    etset

--- src/UserManage.Application/Sessions/Dto/UserLoginInfoDto.cs
+++ src/UserManage.Application/Sessions/Dto/UserLoginInfoDto.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Abp.Application.Services.Dto;
+﻿using Abp.Application.Services.Dto;
 using Abp.AutoMapper;
 using UserManage.Authorization.Users;
 
@@ -15,36 +14,5 @@ namespace UserManage.Sessions.Dto
         public string UserName { get; set; }
 
         public string EmailAddress { get; set; }
-        public string PhoneNumber { get; set; }
-
-        public string SelectDistrict { get; set; }
-
-        public bool IsActive { get; set; }
-
-        public string FullName { get; set; }
-
-        public DateTime? LastLoginTime { get; set; }
-
-        public DateTime CreationTime { get; set; }
-
-        /// <summary>
-        /// 企业微信头像Url
-        /// </summary>
-        public string Avatar { get; set; }
-
-        /// <summary>
-        /// 职务
-        /// </summary>
-        public string Position { get; set; }
-        /// <summary>
-        /// 性别
-        /// </summary>
-        public bool Sex { get; set; }
-        /// <summary>
-        /// 公司id
-        /// </summary>
-
-        public string CompanyId { get; set; }
-
     }
 }
