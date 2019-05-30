a2c840018a79d934eab1d7c73e7b184dcb70db1e
hiuron <hiuron@sina.com>
2019-05-30 16:11:30 +0800

    Revert "etset"
    This reverts commit 05a7885cca9843bda9a2001a1e39bda912b903b2.

--- src/UserManage.Application/Sessions/Dto/UserLoginInfoDto.cs
+++ src/UserManage.Application/Sessions/Dto/UserLoginInfoDto.cs
@@ -1,4 +1,5 @@
-﻿using Abp.Application.Services.Dto;
+﻿using System;
+using Abp.Application.Services.Dto;
 using Abp.AutoMapper;
 using UserManage.Authorization.Users;
 
@@ -14,5 +15,36 @@ namespace UserManage.Sessions.Dto
         public string UserName { get; set; }
 
         public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string SelectDistrict { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string FullName { get; set; }
+
+        public DateTime? LastLoginTime { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+        /// <summary>
+        /// 企业微信头像Url
+        /// </summary>
+        public string Avatar { get; set; }
+
+        /// <summary>
+        /// 职务
+        /// </summary>
+        public string Position { get; set; }
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public bool Sex { get; set; }
+        /// <summary>
+        /// 公司id
+        /// </summary>
+
+        public string CompanyId { get; set; }
+
     }
 }
