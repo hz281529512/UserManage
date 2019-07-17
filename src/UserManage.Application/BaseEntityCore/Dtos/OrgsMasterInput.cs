a71cdb12df48eed83d7f6fe2fed084672c67d14f
Hiuron <hiuron@sina.com>
2019-07-17 10:11:16 +0800

    GetUserListByDistrictRoleAsync

---
+++ src/UserManage.Application/BaseEntityCore/Dtos/OrgsMasterInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserManage.BaseEntityCore.Dtos
+{
+    public class OrgsMasterInput
+    {
+        /// <summary>
+        /// abp 用户ID
+        /// </summary>
+        public long? AbpUserId { get; set; }
+
+        /// <summary>
+        /// F2BPM 主部门ID
+        /// </summary>
+        public string MasterOrgGuid { get; set; }
+    }
+}
--- src/UserManage.Application/Roles/RoleAppService.cs
+++ src/UserManage.Application/Roles/RoleAppService.cs
@@ -147,6 +147,8 @@ namespace UserManage.Roles
                 GrantedPermissionNames = grantedPermissions.Select(p => p.Name).ToList()
             };
         }
+
+        
     }
 }
 
